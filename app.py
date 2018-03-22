018b414af82b92bddf2cb99e5fd5c4e8312e9b7b
hisajarif <muhammed.mi1@iiitmk.ac.in>
2018-03-22 09:50:13 +0530

    upload to heroku

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request
 import tensorflow as tf
 from werkzeug import secure_filename
 import json
@@ -7,6 +7,7 @@ UPLOAD_FOLDER = 'uploaded'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -16,23 +17,12 @@ with tf.gfile.FastGFile('model/output_graph.pb','rb') as f:
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def,name='')
 
-@app.route('/')
+sess = tf.Session()
+label_lines = [line.rstrip() for line in tf.gfile.GFile('model/output_labels.txt')]
+softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+@app.route('/', methods=['POST'])
 def hello():
-    return "Hello World!"
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            print(filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'success!'
-    return 'try again'
-
-@app.route('/predict', methods=['POST'])
-def predict():
     result = ""
     global tf
     if request.method == 'POST':
@@ -42,58 +32,20 @@ def predict():
             print('Received file : '+filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload.jpg'))
             image_data = tf.gfile.FastGFile('uploaded/upload.jpg','rb').read()
-            label_lines = [line.rstrip() for line in tf.gfile.GFile('model/output_labels.txt')]
-            with tf.Session() as sess:
-                softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-                predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0':image_data})
-                top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-                for node_id in top_k:
-                    human_string = label_lines[node_id]
-                    score = predictions[0][node_id]
-                    result = result + '\n'+'%s (score = %.5f)' % (human_string, score)
-                sess.close()
+
+            predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0':image_data})*100
+            top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+            # for node_id in top_k:
+            #     human_string = label_lines[node_id]
+            #     score = predictions[0][node_id]
+            #     result = result + '\n'+'%s (score = %.5f)' % (human_string, score)
+            topScore1 = predictions[0][top_k[0]]
+            result = '%s  =  %2.2f ' % (label_lines[top_k[0]], topScore1)
+            topScore2 = predictions[0][top_k[1]]
+            if topScore1-topScore2 < 30 :
+                result += '\n%s  =  %2.2f ' % (label_lines[top_k[1]], topScore2)
+                pass
     return result
-
-@app.route('/testModel')
-def test_model():
-    result = ""
-    global tf
-    image_data = tf.gfile.FastGFile('model/Arduino.jpg','rb').read()
-    label_lines = [line.rstrip() for line in tf.gfile.GFile('model/output_labels.txt')]
-    with tf.Session() as sess:
-        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-        predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0':image_data})
-        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-        for node_id in top_k:
-            human_string = label_lines[node_id]
-            score = predictions[0][node_id]
-            result = result + '\n'+'%s (score = %.5f)' % (human_string, score)
-        sess.close()
-    return result
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    speech = "Today the weather in INDIA"
-    print("Response:")
-    print(speech)
-
-    res = {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-    res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
 
 if __name__ == '__main__':
     app.run()
